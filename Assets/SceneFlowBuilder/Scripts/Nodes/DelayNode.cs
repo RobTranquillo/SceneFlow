3e180702354f8ce78c41ebab4c229cdc0b7d85fd
Robert Wartenberg <robert@vr-bits.de>
2023-07-05 10:45:54 +0200

    fix DelayNode: take care of all children

--- Assets/SceneFlowBuilder/Scripts/Nodes/DelayNode.cs
+++ Assets/SceneFlowBuilder/Scripts/Nodes/DelayNode.cs
@@ -3,28 +3,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DelayNode : ActionNode
+/// <summary>
+/// DelayNode waits for a given duration before returning success.
+/// All children are started at once after the delay.
+/// </summary>
+public class DelayNode : CompositeNode
 {
     public float duration = 1f;
+    public bool debug = false;
+    
     float startTime;
 
     protected override void OnStart()
     {
         startTime = Time.time;
+        if (debug)
+            Debug.Log("DelayNode started");
     }
 
     protected override void OnStop()
     {
-        child.Update();
-         
-        Stat der ActionNode die keine Kinder besitzt, muss eine neue NNode geschreiben werden,
-         die genau ein Kind haben kann.
+        if (debug)
+            Debug.Log("DelayNode stopped");
     }
 
     protected override State OnUpdate()
     {
-        if (Time.time - startTime > duration)
-            return State.Success;
-        return State.Running;
+        if (Time.time - startTime < duration)
+        {
+            if (debug)
+                Debug.Log("DelayNode running, wait for end of delay");
+            return State.Running;
+        }
+
+        State overAllState = State.Success;
+        foreach (var child in children)
+        {
+            State childState = child.Update();
+
+            if (childState == State.Failure)
+                return State.Failure;
+            
+            if (childState != State.Success)
+                overAllState = State.Running;
+        }
+
+        return overAllState;
     }
 }
